e32e4ce6962fb7b0549d82d7f88c06591df45d1a
Lettever <jrafaelmtavares@gmail.com>
2026-04-30 01:22:14 +0100

    Updated the date logic instead of using date from sqlite, also fixed the updateTotalPlaytime to be atomic

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import sqlite3
 import urllib.parse
 import urllib.request
+from datetime import datetime, timedelta
+
+yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
 
 
 def load_env():
@@ -147,10 +150,9 @@ def addSession(game):
         "SELECT total_playtime from Games where appid = ?", (game["appid"],)
     ).fetchone()[0]
     diff = game["playtime_forever"] - total_playtime
-    data = (game["appid"], diff)
     cur.execute(
-        "INSERT INTO Sessions (appid, playtime, date) VALUES (?, ?, DATE('now', '-1 day'))",
-        data,
+        "INSERT INTO Sessions (appid, playtime, date) VALUES (?, ?, ?)",
+        (game["appid"], diff, yesterday),
     )
     con.commit()
     return diff
@@ -158,32 +160,22 @@ def addSession(game):
 
 def updateTotalPlaytime(game, diff):
     print(f"Updating the game {game['name']} with {diff} minutes")
-    old_total = cur.execute(
-        "SELECT total_playtime from Games where appid = ?", (game["appid"],)
-    ).fetchone()[0]
-    new_total = old_total + diff
-    appid = game["appid"]
     cur.execute(
-        """
-        UPDATE Games
-        SET total_playtime = ?
-        WHERE appid = ?""",
-        (new_total, appid),
+        "UPDATE Games SET total_playtime = total_playtime + ? WHERE appid = ?",
+        (diff, game["appid"]),
     )
     con.commit()
 
 
-# 491m = 8h, 11m
-# 14,7h = 882m = 14h, 42m
-# 882m - 491m = 391m = 6h, 31m
-
-
 def printDailySummary():
-    cur.execute("""
+    cur.execute(
+        """
             SELECT g.name, s.playtime
             FROM Sessions s
             JOIN Games g ON s.appid = g.appid
-            WHERE s.date = DATE('now', '-1 day')""")
+            WHERE s.date = ?""",
+        (yesterday,),
+    )
 
     sessions = cur.fetchall()
     if sessions:
@@ -208,34 +200,3 @@ if __name__ == "__main__":
                 diff_time = addSession(game)
                 updateTotalPlaytime(game, diff_time)
     printDailySummary()
-
-"""
-# getOwnedGames will only be used once to populate the db
-getOwnedGames example
-{
-    "appid":367520,
-    "name":"Hollow Knight",
-    "playtime_forever":1525,
-    "img_icon_url":"f6ab055c2366237200b1a31cccbd6cf81e436d72",
-    "has_community_visible_stats":true,
-    "playtime_windows_forever":996,
-    "playtime_mac_forever":0,
-    "playtime_linux_forever":528,
-    "playtime_deck_forever":0,
-    "rtime_last_played":1767729208,
-    "playtime_disconnected":17
-}
-
-getRecentGames example
-{
-    "appid":1030300,
-    "name":"Hollow Knight: Silksong",
-    "playtime_2weeks":627,
-    "playtime_forever":5410,
-    "img_icon_url":"b4a999c1302e3ac123c041fd41bb8a34528c6ab5",
-    "playtime_windows_forever":4035,
-    "playtime_mac_forever":0,
-    "playtime_linux_forever":1375,
-    "playtime_deck_forever":0
-}
-"""
